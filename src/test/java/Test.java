8185f99ea9695f7dda85beb205e28ed3800cbeb1
Steven Oberlander <saoberlander@gmail.com>
2024-11-05 22:15:18 -0500

    third commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("this is the project version 2");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("this is the project version 3");
+
 
     }
 }
